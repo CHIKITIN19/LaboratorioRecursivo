d98f1147976052e306b9364355322fa569e73a56
CHIKITIN19 <141179159+CHIKITIN19@users.noreply.github.com>
2024-10-11 18:53:09 -0600

    Se programa la clase tablero aun pendiente si hay que agregar algo mas

--- AjedrezRecursivo/Tablero.java
+++ AjedrezRecursivo/Tablero.java
@@ -9,5 +9,34 @@ package AjedrezRecursivo;
  * @author 9567
  */
 public class Tablero {
+    private int [][] tablero;
+    private static final int tamano = 0;
     
+    public Tablero(){
+        tablero = new int [tamano][tamano];
+        for (int i = 0; i < tamano; i++) {
+            for (int j = 0; j < tamano; j++) {
+                tablero[i][j] = -1;
+            }
+        }
+    }
+    public boolean MovimientoValido(int x, int y ){
+        return (x>=0 && x < tamano && y>= 0 && y< tamano && tablero[x][y] == -1);
+    }
+    public void Posicion(int x, int y, int paso){
+        tablero[x][y] = paso;
+    }
+    public void MostrarTablero(){
+        for (int i = 0; i < tamano; i++) {
+            for (int j = 0; j < tamano; j++) {
+                if (tablero[i][j] == -1) {
+                    System.out.println("X"); //Marca las casillas con X
+                }else{
+                    System.out.printf(" %2d ", tablero[i][j]);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
